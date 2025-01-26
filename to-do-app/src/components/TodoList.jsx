54ff9f4c6a8999c1b027c1aa4d641ce58159f38d
Chadzinho <chadwilliams810@gmail.com>
2025-01-26 13:59:28 +0900

    Add TodoCards

--- to-do-app/src/components/TodoList.jsx
+++ to-do-app/src/components/TodoList.jsx
@@ -1,9 +1,21 @@
 import { TodoCard } from "./TodoCard";
 
-export function TodoList() {
-    return (
-        <div>
-            <TodoCard />
-        </div>
-    )
+export function TodoList(props) {
+  const { todos } = props;
+
+  const tab = "Completed";
+  const filterTodosList =
+    tab === "All"
+      ? todos
+      : todos === "Completed"
+      ? todos.filter((val) => val.complete)
+      : todos.filter((val) => !val.complete);
+
+  return (
+    <>
+      {filterTodosList.map((todo, todoIndex) => {
+        return <TodoCard key={todoIndex} todoIndex={todoIndex} todo={todo} />;
+      })}
+    </>
+  );
 }
